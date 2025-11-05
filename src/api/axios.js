2abb7f50f337f8efa07cf7d1d7c68c19d2e36194
ishratjahanesha21 <ishratjahanesha21@gmail.com>
2025-11-05 12:37:03 +0600

    add vercel config

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with default config
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL ,
   headers: {
     'Content-Type': 'application/json',
   },
